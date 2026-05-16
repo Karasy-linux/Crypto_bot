238a32351642801d705980608b3a335da187a6d8
Karasy <venbest1992@gmail.com>
2026-05-16 19:37:49 +0300

    update seed_db.py

--- seed_db.py
+++ seed_db.py
@@ -6,8 +6,8 @@ from get_data import QUERIES
 def init_db(db = 'data.db') -> None:
     with sqlite3.connect(db) as con:
         query = QUERIES.get("tables")
+        
         cur = con.cursor()
-
         cur.executescript(query)
 
 
@@ -16,6 +16,7 @@ def init_db(db = 'data.db') -> None:
 def seed_db(db = 'data.db') -> None:
     with sqlite3.connect(db) as con:
         query = "INSERT OR IGNORE INTO assets (id, name) VALUES (?, ?)"
+
         cur = con.cursor()
         cur.executemany(query,(COINS))    
 
